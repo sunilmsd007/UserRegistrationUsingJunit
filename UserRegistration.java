10fc3fce62f969dcaae408909ec7f15ba7f014f9
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-29 22:42:51 +0530

    Modified: refactored email regex pattern to take multiple entries

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,29 +4,52 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
+        //method to validate first name
 	public boolean validateFirstName(String firstname) {
 		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
 		Matcher matcher = pattern.matcher(firstname);
 		return matcher.matches();
 	}
+        //method to validate last name
         public boolean validateLastName(String lastname) {
 		Pattern pattern= Pattern.compile("[A-Z][a-z]{2,}");
 		Matcher matcher = pattern.matcher(lastname);
 		return matcher.matches();
 	}
+        //method to validate email
 	public boolean validateEmail(String email) {
-		Pattern pattern= Pattern.compile("(abc)[.]?[a-z]*@(bl.co)[.]?[a-z]{0,2}$");
+		Pattern pattern= Pattern.compile("^(abc)[-.+0-9a-z]*@[a-z0-9]+[.][a-z]{0,3}[.]?[a-z]{0,2}$");
 		Matcher matcher = pattern.matcher(email);
 		return matcher.matches();
 	}
+        //method to validate phone number
 	public boolean validatePhoneNumber(String phonenumber) {
 		Pattern pattern = Pattern.compile("[0-9]{2} [0-9]{10}");
 		Matcher matcher = pattern.matcher(phonenumber);
 		return matcher.matches();
 	}
+        //method to validate password
         public boolean validatePassWord(String password) {
                 Pattern pattern = Pattern.compile("[A-Z]+[0-9]+[@$&#]{1}\\w{5,}");
                 Matcher matcher = pattern.matcher(password);
                 return matcher.matches();
         }
+	public static void main(String[] args) {
+        	UserRegistration user = new UserRegistration();
+				
+		System.out.println(user.validateFirstName("Sunil"));;
+		System.out.println(user.validateLastName("Kumar"));
+		System.out.println(user.validateEmail("abc.xyz@bl.co.in"));
+		System.out.println(user.validatePhoneNumber("91 9164144279"));
+		System.out.println(user.validatePassWord("A1@aS7ft"));
+		//validate multiple email entries
+		ArrayList<String> emails = new ArrayList<>();
+		emails.add("abc.xyz@bl.co.in");
+		emails.add("abc.100@yahoo.com");
+		emails.add("abc111@abc.com");
+		emails.add("abc@abc.net.au");
+		for(String str: emails) {
+			System.out.println(user.validateEmail(str));
+			}
+		}
 }
